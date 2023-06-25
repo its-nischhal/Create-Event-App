79b42a5c74f5e8fa4277100a80a678574856b7ae
Nischhal Sharma <ppriyanshi104@gmail.com>
2023-06-25 15:31:29 +0530

    [Add] UnderDevelopmentScreen

---
+++ lib/app/view/under_development_screen/under_development_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UnderDevelopmentScreen extends StatelessWidget {
+  const UnderDevelopmentScreen({super.key});
+
+  @override
+  Scaffold build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Comming Soon')),
+    );
+  }
+}
